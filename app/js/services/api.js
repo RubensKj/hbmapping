0f5b1d9aaa16450c5250d5fdd0538d0738da5c28
gustavo.martins <gustavofmartins44@gmail.com>
2020-02-10 15:00:41 -0300

    setting up for deploy

--- app/js/services/api.js
+++ app/js/services/api.js
@@ -1,5 +1,5 @@
 angular.module('app').factory('RestangularRoute', ["Restangular", function (Restangular) {
   return Restangular.withConfig(function (RestangularConfigurer) {
-      RestangularConfigurer.setBaseUrl('http://localhost:8080/api');
+      RestangularConfigurer.setBaseUrl('https://infinite-garden-63209.herokuapp.com/api');
   });
 }]);
